e56c0a85f36f6d05a55f970015c92e6712005bd1
Sreekanth Jaladanki <139740711+SreekanthJaladanki@users.noreply.github.com>
2024-04-23 13:16:30 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ class Demo{
 	public static void main(String [] args){
 		System.out.println("Hello All");
 		System.out.println("Hello Jenkins");
-		System.out.println("Hello Git");
+		System.out.println("Hello Amar");
 	}
 }
 

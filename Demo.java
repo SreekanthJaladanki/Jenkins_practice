146cca33e16518188691a8764b16cb3b749c3c34
brahmeswaramma <150504340+brahmeswaramma@users.noreply.github.com>
2024-09-28 18:49:37 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ class Demo{
 	public static void main(String [] args){
 		System.out.println("Hello All");
 		System.out.println("Hello Jenkins");
-		System.out.println("Hello DevOps");
+		System.out.println("Hello AWS");
 	}
 }
 

43da922d69a61f621d8f3883e1a3447708a498a2
Sreekanth Jaladanki <139740711+SreekanthJaladanki@users.noreply.github.com>
2025-08-25 11:46:34 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,7 @@
 class Demo{
 	public static void main(String [] args){
-		System.out.println("Hello All");
+		System.out.println("Hello from SIT");
 		System.out.println("Hello Jenkins");
-		System.out.println("Hello DevOps");
-		System.out.println("Hello ");
-	}
+		
 }
 
